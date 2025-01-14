23fb16d505cb8b55c11a05718477a06273afdab3
Petro <petrotymchuck@gmail.com>
2025-01-14 08:06:46 +0200

    Demo 1.0

---
+++ Client/Connecting/ClientMessageSender.cs
@@ -0,0 +1,65 @@
+﻿using Client.Models;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+
+namespace Client.Connecting
+{
+    public static class ClientMessageSender
+    {
+        public static Paragraph CreateMessageText(UserView user, string message, HttpClient imageServer)
+        {
+            Run userText = new Run($"{user.Name}: ") { Foreground = System.Windows.Media.Brushes.Blue };
+            Run messageText = new Run($"{message}\n");
+
+            Paragraph paragraph = new Paragraph();
+
+            paragraph.Inlines.Add(CreateImageForMessage(user.AvatarImgPath, 40, imageServer));
+            paragraph.Inlines.Add(userText);
+            paragraph.Inlines.Add(messageText);
+
+            return paragraph;
+        }
+
+        public static async Task<Paragraph> CreateMessageImage(UserView user, string imagePath, HttpClient imageServer)
+        {
+            Run userText = new Run($"{user.Name}: ") { Foreground = System.Windows.Media.Brushes.Blue };
+
+            Paragraph paragraph = new Paragraph();
+
+            paragraph.Inlines.Add(CreateImageForMessage(user.AvatarImgPath, 40, imageServer));
+            paragraph.Inlines.Add(userText);
+            paragraph.Inlines.Add(CreateImageForMessage(await ImageSender.UploadImageAsync(imagePath, imageServer), 100, imageServer));
+
+            return paragraph;
+        }
+
+        private static InlineUIContainer CreateImageForMessage(string serverImagePath, int height, HttpClient imageServer) 
+        {
+            System.Windows.Controls.Image avatarImage = new System.Windows.Controls.Image();
+
+            BitmapImage bitmap = ImageSender.GetImageFromServer(
+                serverImagePath,
+                imageServer
+            );
+
+            double originalWidth = bitmap.PixelWidth;
+            double originalHeight = bitmap.PixelHeight;
+            double targetHeight = height;
+            double aspectRatio = originalWidth / originalHeight;
+
+            avatarImage.Height = targetHeight;
+            avatarImage.Width = targetHeight * aspectRatio;
+            avatarImage.Source = bitmap;
+            avatarImage.Margin = new System.Windows.Thickness(5,0,5,0);
+
+            return new InlineUIContainer(avatarImage);
+        }
+    }
+}
--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -55,19 +55,26 @@ namespace Client
 
                 _hubConnection.On<string, string, string>("ReceiveMessage", (userName, message, imagePath) =>
                 {
-                    Dispatcher.Invoke(() =>
+                    Dispatcher.Invoke(async () =>
                     {
                         UserView anotherUser = new UserView() { Name = userName, AvatarImgPath = imagePath };
-                        AppendTextToRichTextBox(anotherUser, message);
+                        if (message.Contains("/") || message.Contains("\\"))
+                        {
+                            await AppendImageMessage(anotherUser, message);
+                        }
+                        else
+                        {
+                            AppendTextMessage(anotherUser, message);
+                        }
                     });
                 });
 
                 await _hubConnection.StartAsync();
-                AppendTextToRichTextBox(_serverInChat, "Connected to the chat server");
+                AppendTextMessage(_serverInChat, "Connected to the chat server");
             }
             catch (Exception ex)
             {
-                AppendTextToRichTextBox(_serverInChat, $"Connection failed: {ex.Message}");
+                AppendTextMessage(_serverInChat, $"Connection failed: {ex.Message}");
             }
         }
 
@@ -80,11 +87,12 @@ namespace Client
                     throw new Exception("Invalid server");
                 }
 
+                string message = this.messageInputTB.Text;
+
                 if (sendBT.Content.ToString() == _sendBtText)
                 {
                     if (_hubConnection.State == HubConnectionState.Connected)
                     {
-                        string message = this.messageInputTB.Text;
 
                         if (!string.IsNullOrEmpty(message))
                         {
@@ -93,12 +101,12 @@ namespace Client
                     }
                     else
                     {
-                        AppendTextToRichTextBox(_serverInChat, "Not connected to the server.");
+                        AppendTextMessage(_serverInChat, "Not connected to the server.");
                     }
                 }
                 else
                 {
-                    await SendImage(_user, this.messageInputTB.Text);
+                    await _hubConnection.InvokeAsync("SendMessage", _user.Name, message, _user.AvatarImgPath);
                 }
 
                 this.sendBT.Content = _sendBtText;
@@ -106,7 +114,7 @@ namespace Client
             }
             catch (Exception ex)
             {
-                AppendTextToRichTextBox(_serverInChat, ex.Message);
+                AppendTextMessage(_serverInChat, ex.Message);
             }
         }
 
@@ -131,84 +139,18 @@ namespace Client
             }
         }
 
-        private void AppendTextToRichTextBox(UserView user, string message)
+        private void AppendTextMessage(UserView user, string message)
         {
-            Run userText = new Run($"{user.Name}: ") { Foreground = System.Windows.Media.Brushes.Blue };
-            Run messageText = new Run($"{message}\n");
-
-            System.Windows.Controls.Image avatarImage = new System.Windows.Controls.Image();
-
-            BitmapImage bitmap = ImageSender.GetImageFromServer(
-                user.AvatarImgPath,
-                _imageServer
-            );
-
-            double originalWidth = bitmap.PixelWidth;
-            double originalHeight = bitmap.PixelHeight;
-            double targetHeight = 100;
-            double aspectRatio = originalWidth / originalHeight;
-
-            avatarImage.Height = targetHeight;
-            avatarImage.Width = targetHeight * aspectRatio;
-            avatarImage.Source = bitmap;
-
-            InlineUIContainer container = new InlineUIContainer(avatarImage);
-
-            Paragraph paragraph = new Paragraph();
-            paragraph.Inlines.Add(container);
-            paragraph.Inlines.Add(userText);
-            paragraph.Inlines.Add(messageText);
-
-            this.chatMessagesTB.Document.Blocks.Add(paragraph);
+            this.chatMessagesTB.Document.Blocks.Add(ClientMessageSender.CreateMessageText(user, message, _imageServer));
 
             this.chatMessagesTB.ScrollToEnd();
         }
 
-        private async Task SendImage(UserView user, string imagePath)//доробити
+        private async Task AppendImageMessage(UserView user, string imagePath)
         {
-            Run userText = new Run($"{user.Name}: ") { Foreground = System.Windows.Media.Brushes.Blue };
-
-            System.Windows.Controls.Image avatarImage = new System.Windows.Controls.Image();
-
-            BitmapImage bitmap = ImageSender.GetImageFromServer(
-                user.AvatarImgPath,
-                _imageServer
-            );
-
-            double originalWidth = bitmap.PixelWidth;
-            double originalHeight = bitmap.PixelHeight;
-            double targetHeight = 100;
-            double aspectRatio = originalWidth / originalHeight;
-
-            avatarImage.Height = targetHeight;
-            avatarImage.Width = targetHeight * aspectRatio;
-            avatarImage.Source = bitmap;
-
-            InlineUIContainer avatarContainer = new InlineUIContainer(avatarImage);
-
-            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
-
-            bitmap = ImageSender.GetImageFromServer(
-                await ImageSender.UploadImageAsync(imagePath, _imageServer),
-                _imageServer
-            );
-
-            originalWidth = bitmap.PixelWidth;
-            originalWidth = bitmap.PixelHeight;
-            originalWidth = 100;
-            originalWidth = originalWidth / originalHeight;
-
-            image.Height = targetHeight;
-            image.Width = targetHeight * aspectRatio;
-            image.Source = bitmap;
-
-            InlineUIContainer container = new InlineUIContainer(image);
-            Paragraph paragraph = new Paragraph();
-            paragraph.Inlines.Add(avatarContainer);
-            paragraph.Inlines.Add(userText);
-            paragraph.Inlines.Add(container);
-
-            this.chatMessagesTB.Document.Blocks.Add(paragraph);
+            this.chatMessagesTB.Document.Blocks.Add(await ClientMessageSender.CreateMessageImage(user, imagePath, _imageServer));
+            
+            this.chatMessagesTB.ScrollToEnd();
         }
     }
 }
